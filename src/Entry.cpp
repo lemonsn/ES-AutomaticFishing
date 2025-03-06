2dc6c9df5ef2dd29b6723ce7c2409f1429a62dc9
zimuya4153 <limulu10@163.com>
2025-03-06 19:41:06 +0800

    refactor: use SIGNATURE_CALL macro for function resolving

--- src/Entry.cpp
+++ src/Entry.cpp
@@ -10,25 +10,18 @@ GLACIE_INSTANCE_HOOK(
     auto result = origin();
     if (result && !glacie::memory::dAccess<int>(this, 976)) { // FishingHook::mTimeUntilHooked
         // FishingHook::getOwner
-        static auto getOwnerFunc = reinterpret_cast<void* (*)(void* self)>(
-            glacie::memory::resolveIdentifier("48 89 5C 24 10 57 48 83 EC 20 48 8B B9 D8 01 00 00")
-        );
+        static auto getOwnerFunc = SIGNATURE_CALL("48 89 5C 24 10 57 48 83 EC 20 48 8B B9 D8 01 00 00", void*, void*);
 
         // Player::getSelectedItem
-        static auto getSelectedItemFunc = reinterpret_cast<void* (*)(void* self)>(glacie::memory::resolveIdentifier(
-            "48 8B 91 C8 05 00 00 80 BA B0 00 00 00 00 75 18 48 8B 8A B8 00 00 00 8B 52 10 48 8B 01 48 8B 40 38 48 FF "
-            "25 30 37 C6 01"
-        ));
+        static auto getSelectedItemFunc =
+            SIGNATURE_CALL("48 8B 91 C8 05 00 00 80 BA B0 00 00 00 00 75 18 48 8B 8A B8 00 00 00 8B 52 10 48 8B 01 48 8B 40 38 48 FF 25 30 37 C6 01", void*, void*);
 
         // ItemStackBase::getItem
-        static auto getItemFunc = reinterpret_cast<void* (*)(void* self)>(
-            glacie::memory::resolveIdentifier("48 8B 41 08 48 85 C0 74 04 48 8B 00 C3")
-        );
+        static auto getItemFunc = SIGNATURE_CALL("48 8B 41 08 48 85 C0 74 04 48 8B 00 C3", void*, void*);
 
         // ItemStackBase::isNull
-        static auto isNullFunc = reinterpret_cast<bool (*)(void* self)>(glacie::memory::resolveIdentifier(
-            "48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 48 89 7C 24 ?? 41 56 48 83 EC ?? 80 79 ?? ?? 48 8B D9"
-        ));
+        static auto isNullFunc =
+            SIGNATURE_CALL("48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 48 89 7C 24 ?? 41 56 48 83 EC ?? 80 79 ?? ?? 48 8B D9", bool, void*);
 
         if (auto* player = reinterpret_cast<void*>(getOwnerFunc(this)); player != nullptr) {
             auto* itemStack = getSelectedItemFunc(player);
